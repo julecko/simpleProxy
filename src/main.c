554989b203bc9c4a5f121df31f84e85c4ec0c2cc
Julecko <julecko@dilino.sk>
2025-06-16 13:57:33 +0200

    Added simple thread creation and deletion to allow multiple connections at once

--- src/main.c
+++ src/main.c
@@ -1,6 +1,27 @@
 #include "proxy.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
+
+#define MAX_CONNECTIONS 20
+
+int active_threads = 0;
+pthread_mutex_t count_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *client_thread(void *arg) {
+    int client_sock = *(int*)arg;
+    free(arg);
+
+    handle_client(client_sock);
+    close(client_sock);
+
+    pthread_mutex_lock(&count_mutex);
+    active_threads--;
+    pthread_mutex_unlock(&count_mutex);
+
+    return NULL;
+}
 
 int main() {
     int server_sock = create_server_socket(8080, 5);
@@ -9,10 +30,39 @@ int main() {
     printf("Proxy server running on port 8080...\n");
 
     while (1) {
-        int client_sock = accept_connection(server_sock);
-        if (client_sock < 0) continue;
+        pthread_mutex_lock(&count_mutex);
+        int current_active = active_threads;
+        pthread_mutex_unlock(&count_mutex);
 
-        handle_client(client_sock);
+        if (current_active >= MAX_CONNECTIONS) {
+            sleep(1);
+            continue;
+        }
+
+        int *client_sock_ptr = malloc(sizeof(int));
+        if (!client_sock_ptr) {
+            perror("malloc");
+            continue;
+        }
+        *client_sock_ptr = accept_connection(server_sock);
+        if (*client_sock_ptr < 0) {
+            free(client_sock_ptr);
+            continue;
+        }
+
+        pthread_t tid;
+        if (pthread_create(&tid, NULL, client_thread, client_sock_ptr) != 0) {
+            perror("pthread_create");
+            close(*client_sock_ptr);
+            free(client_sock_ptr);
+            continue;
+        }
+
+        pthread_detach(tid);
+
+        pthread_mutex_lock(&count_mutex);
+        active_threads++;
+        pthread_mutex_unlock(&count_mutex);
     }
 
     close(server_sock);
