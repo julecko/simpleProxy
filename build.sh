53242ba87fcc3de9b071c4664d390edc7fa079d5
Julecko <julkoklein@gmail.com>
2025-06-25 14:53:33 +0200

    Updated .env files and building of cmake project

--- build.sh
+++ build.sh
@@ -1,7 +1,20 @@
-mkdir -p build
-cd build
+#!/bin/bash
 
-cmake ..
+set -e
+
+BUILD_TYPE="Debug"
+
+if [[ "$1" == "--release" ]]; then
+  BUILD_TYPE="Release"
+fi
+
+BUILD_DIR="build/${BUILD_TYPE,,}"
+
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../..
 make
 
+echo "Running ./simpleProxy ($BUILD_TYPE build)"
 ./simpleProxy
